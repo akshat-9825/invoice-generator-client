bd978d61a34d8966826c469fbea173f62b41d5cb
Akshat-Dwivedi <akshat10dwivedi@gmail.com>
2024-03-14 16:12:29 +0530

    log added

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,6 +8,8 @@ export const BASE_URL = () => {
   if (import.meta.env.DEV) {
     return import.meta.env.VITE_REACT_APP_BASE_API_URL;
   } else {
+    console.log(import.meta.env.VITE_PRODUCTION_BASE_API_URL, " import one");
+    console.log(process.env.VITE_PRODUCTION_BASE_API_URL, "process.env");
     return import.meta.env.VITE_PRODUCTION_BASE_API_URL;
   }
 };
